d578a7d87cc8d12553c2b7d0febab0922f9ff495
ianYuinCellcubes <168510453+ianYuinCellcubes@users.noreply.github.com>
2025-01-09 15:22:35 +0900

    Update main.py

--- source/player/main.py
+++ source/player/main.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append("/home/cellcubes/Desktop/player")
+sys.path.append("/home/cellcubes/Desktop/player/C2_GH_ES2025")
 from PySide6.QtWidgets import QApplication
 from source.player.Control.mainControl import MainController
 
